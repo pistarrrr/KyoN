a67710c438f748ff14ce759dd565c614c4ba74b0
pistarrrr <codewithvistar@gmail.com>
2025-05-08 13:50:32 +0700

    Initial Commit

---
+++ KyoN/Models/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModels.swift
+//  KyoN
+//
+//  Created by Vistar Tiop R on 08/05/25.
+//
+
+import SwiftData
---
+++ KyoN/ViewModels/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  KyoN
+//
+//  Created by Vistar Tiop R on 08/05/25.
+//
+
+import SwiftUI
